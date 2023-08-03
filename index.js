53f962b92eaff7d7ba98d8df4e58924704d2b72c
KristyKostyor <ms.kostyor@gmail.com>
2023-08-03 21:22:21 +0300

    debounce

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
+const  debounce = (func, wait) => {
+  let timeout;
 
-const container = document.createElement('div');
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
+
+const container = document.createElement("div");
 container.classList.add("container");
 
-const input = document.createElement('input');
-input.classList.add('one');
+const input = document.createElement("input");
+input.classList.add("one");
 
-const par = document.createElement('p');
-par.textContent='';
-par.classList.add('two');
+const par = document.createElement("p");
+par.textContent = "";
+par.classList.add("two");
 
+const debouncedUpdate = debounce(() => {
+  par.textContent = input.value;
+}, 3000);
 
-input.addEventListener('input', ()=> {
-    
-    input.timer=setTimeout(() => {
-        par.textContent=input.value;
-    }, 3000);
-})
-
-
-
+input.addEventListener("input", debouncedUpdate);
 
 document.body.appendChild(container);
 container.appendChild(input);
 container.appendChild(par);
-
